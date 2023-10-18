d72a56852affb91caeb59b093675b8be499a0f74
MateoLCS <mateusz.namysl0318@gmail.com>
2023-10-18 22:08:48 +0200

    Implemented the rest linked list methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,7 +6,13 @@ require_relative 'node'
 #size - returns the total number of nodes in the list
 #head - returns the first node in the list
 #tail - returns the last node in the list
-
+#at(index) - returns the node at the given index
+#pop - removes the last element from the list
+#contains?(value) - returns true if the passed in value is in the list and otherwise returns false.
+#find(value) - returns the index of the node containing value, or nil if not found.
+#to_s - returns a string representation of the list
+#insert_at(index, value) -  inserts given value at the given index
+#remove_at(index) - removes node at given index.
 
 
 class LinkedList
@@ -57,6 +63,76 @@ class LinkedList
     @tail.value unless @tail.nil?
   end
 
+  def contains?(value)
+    return false if @head.nil?
+
+    node = @head
+    until node == @tail
+      return true if node.value == value
+
+      node = node.next_node
+    end
+    node.value == value
+  end
+
+  def at(index)
+    return nil if @head.nil?
+
+    node = @head
+    index.times do
+      node = node.next_node
+    end
+    node.value
+  end
+
+  def pop
+    return nil if @head.nil?
+
+    node = @head
+    until node.next_node == @tail
+      node = node.next_node
+    end
+    node.next_node = nil
+    @tail = node
+  end
+
+  def find(value)
+    return nil if @head.nil?
+      
+      node = @head
+      index = 0
+      until node == @tail
+        return index if node.value == value
+  
+        node = node.next_node
+        index += 1
+      end
+      node.value == value ? index : nil
+  end
+
+  def insert_at(index, value)
+    return nil if @head.nil?
+
+    node = @head
+    (index - 1).times do
+      node = node.next_node
+    end
+
+    new_node = Node.new(value)
+    new_node.next_node = node.next_node
+    node.next_node = new_node
+  end
+
+  def remove_at(index)
+    return nil if @head.nil?
+
+    node = @head
+    (index - 1).times do
+      node = node.next_node
+    end
+    node.next_node = node.next_node.next_node
+  end
+
   def to_s
     return nil if @head.nil?
 
@@ -65,6 +141,7 @@ class LinkedList
       print "( #{node.value} ) -> "
       node = node.next_node
     end
-    print "( #{node.value} ) -> nil"
+    puts "( #{node.value} ) -> nil"
+    
   end
 end
--- main.rb
+++ main.rb
@@ -3,6 +3,13 @@ require './lib/linked_list'
 list = LinkedList.new
 list.append(1)
 list.append(2)
-list.prepend(3)
-list.prepend(4)
+list.append(3)
+list.append(4)
+list.append(5)
+list.to_s
+p list.find(4)
+p list.find(6)
+list.insert_at(3,6)
+list.to_s
+list.remove_at(3)
 list.to_s
